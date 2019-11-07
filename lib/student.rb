a345c64275c969ba650aacf156c243c5c508216b
James Mobus <mobusj@live.com>
2019-11-07 23:57:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,5 +83,9 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 10 LIMIT ?;
       SQL
+      
+      DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
